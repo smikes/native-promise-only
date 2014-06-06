edc987e5bb9500a7337a0c9b647262cd1b264d3e
smikes <smikes@cubane.com>
2014-06-06 17:31:48 -0600

    test: add 2-element case, full coverage for Promise.all

--- test/all.js
+++ test/all.js
@@ -13,6 +13,29 @@ function unexpectedReject(err) {
     throw "Unexpected reject " + err;
 }
 
+function resolveImmediately(arg) {
+    return function (resolve, reject) {
+	setImmediate(function () { resolve(arg); });
+    };
+}
+
+function rejectImmediately(arg) {
+    return function (resolve, reject) {
+	setImmediate(function () { reject(arg); });
+    };
+}
+
+function resolveAfter(time, arg) {
+    return function (resolve, reject) {
+	setTimeout(function () { resolve(arg); }, time);
+    };
+}
+
+function rejectAfter(time, arg) {
+    return function (resolve, reject) {
+	setTimeout(function () { reject(arg); }, time);
+    };
+}
 
 // behavior from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise#Static_methods
 describe("ES6 Promise.all", function () {
@@ -21,6 +44,15 @@ describe("ES6 Promise.all", function () {
     });
     it("should take a single, iterable argument", function () {
 	assert.equal(Promise.all.length,1);
+    });
+    
+    it("should immediately reject for non-Array argument", function (done) {
+	Promise.all({}).then(unexpectedResolve, function (err) {
+	    assert.ok(err instanceof TypeError);
+	    assert.equal("Not an array", err.message);
+	    done();
+	});
+
     });
 
     // 0 elements
@@ -41,9 +73,7 @@ describe("ES6 Promise.all", function () {
     describe("with 1-element array", function () {
 	// 1 element
 	it("should accept an array of one promise", function (done) {
-	    var p1 = new Promise(function (resolve, reject) {
-		setImmediate(function() { resolve(1); });
-	    });
+	    var p1 = new Promise(resolveImmediately(1));
 	    
 	    Promise.all([p1]).then(function (resolved) {
 		assert.equal(resolved, 1);
@@ -53,9 +83,7 @@ describe("ES6 Promise.all", function () {
 	it("should resolve immediately", function (done) {
 	    var obj = {};
 	    
-	    var p1 = new Promise(function (resolve, reject) {
-		setImmediate(function () { resolve(obj); });
-	    });
+	    var p1 = new Promise(resolveAfter(10, obj));
 
 	    Promise.all([p1]).then(function (resolved) {
 		assert.equal(resolved[0], obj);
@@ -65,9 +93,7 @@ describe("ES6 Promise.all", function () {
 	it("should reject immediately", function (done) {
 	    var obj = {};
 	    
-	    var p1 = new Promise(function (resolve, reject) {
-		setImmediate(function () { reject(obj); });
-	    });
+	    var p1 = new Promise(rejectImmediately(obj));
 
 	    Promise.all([p1]).then(unexpectedResolve, function (err) {
 		assert.equal(err, obj);
@@ -78,10 +104,59 @@ describe("ES6 Promise.all", function () {
     
     // 2 elements
     describe("with 2-element array", function () {
-	it("should accept an array of two promises");
-	it("should not resolve immediately when one of a two-promise-array resolves");
-	it("should resolve after both promises of a two-promise-array resolve");
-	it("should reject immediately when the first member of a two-promise-array rejects");
-	it("should reject immediately when the second member of a two-promise-array rejects");
+	it("should accept an array of two promises", function (done) {
+	    var p1 = new Promise(resolveImmediately(1)),
+		p2 = new Promise(resolveImmediately(2));
+		
+	    Promise.all([p1, p2]).then(function (resolved) {
+		assert.deepEqual(resolved, [1, 2]);
+		done();
+	    }, unexpectedReject);
+
+	});
+
+	it("should not resolve immediately when one of a two-promise-array resolves", function (done) {
+	    var p1 = new Promise(resolveAfter(10, 1)),
+		p2 = new Promise(resolveAfter(20, 2));
+
+	    var allResolved = false;
+
+	    Promise.all([p1, p2]).then(function (resolved) {
+		allResolved = true;
+		done();
+	    }, unexpectedReject);
+
+	    p1.then(function (resolved) {
+		assert.equal(resolved, 1);
+		assert.equal(allResolved, false);
+	    });
+
+	    p2.then(function (resolved) {
+		assert.equal(resolved, 2);
+		assert.equal(allResolved, true);
+	    });
+	});
+// covered by case above
+//	it("should resolve after both promises of a two-promise-array resolve");
+
+	it("should reject immediately when the first member of a two-promise-array rejects", function (done) {
+	    var p1 = new Promise(rejectAfter(10, 1)),
+		p2 = new Promise(resolveAfter(20, 2));
+	    
+	    Promise.all([p1, p2]).then(unexpectedResolve, function (err) {
+		assert.equal(err, 1);
+		done();
+	    });
+	});
+
+	it("should reject immediately when the second member of a two-promise-array rejects", function (done) {
+	    var p1 = new Promise(resolveAfter(10, 1)),
+		p2 = new Promise(rejectAfter(20, 2));
+	    
+	    Promise.all([p1, p2]).then(unexpectedResolve, function (err) {
+		assert.equal(err, 2);
+		done();
+	    });
+	});
     });
 });
